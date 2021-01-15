d5de18ad948c875975dfc3f84842d648269797d3
Ant-Mac <antonymitchell@hotmail.com>
2021-01-15 19:42:52 +0000

    added basic recoil implementation for a dark mode button

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,17 +1,18 @@
-import { useMediaQuery } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { useMemo } from "react";
+import { useRecoilState } from "recoil";
+import { preferDarkState } from "../atoms";
 
 const Theme =({ children }: { children: React.ReactNode }) => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [preferDark] = useRecoilState(preferDarkState);
 
   const theme = useMemo(() =>createMuiTheme({
     // TODO: Hugh: This is where the theme settings go
     // Check https://material-ui.com/customization/theming/
     palette: {
-      type: prefersDarkMode ? "dark" : "light"
+      type: preferDark ? "dark" : "light"
     }
-  }), [prefersDarkMode]);
+  }), [preferDark]);
 
   return (
     <ThemeProvider theme={theme}>{children}</ThemeProvider>
