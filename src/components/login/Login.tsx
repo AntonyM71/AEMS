d299026644978c366b95c59c10cd392af5d69dc6
Wozzo <w.n.bates@gmail.com>
2021-01-15 22:47:55 +0000

    Add image of kayaker

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -28,7 +28,7 @@ const useStyles = makeStyles(theme => ({
     height: "100vh"
   },
   image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundImage: "url(/images/HurleyClassic2018/IMGP6327.png)",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light" ? theme.palette.grey[50] : theme.palette.grey[900],
