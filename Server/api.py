45dff9329ee42a7062027a9499702259fda04d95
Ant-Mac <antonymitchell@hotmail.com>
2022-09-10 11:10:10 +0100

    Working autoapi

--- Server/api.py
+++ Server/api.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
-from fastapi_quickcrud import crud_router_builder
+from fastapi_quickcrud.crud_router import generic_sql_crud_router_builder
 
-from db.models import Bug
+from db.models import Child, Parent
 
-crud_route_1 = crud_router_builder(
-    db_model=Bug,
-    prefix="/bug",
-    tags=["Bug"],
-    async_mode=True
+crud_route_parent = generic_sql_crud_router_builder(
+    db_model=Parent,
+    prefix="/parent",
+    tags=["parent"],
+)
 
-    )
-
+crud_route_child = generic_sql_crud_router_builder(
+    db_model=Child,
+    prefix="/child",
+    tags=["child"]
+)
 
 app = FastAPI()
-app.include_router(crud_route_1)
+[app.include_router(i) for i in [crud_route_parent, crud_route_child]]
+
+@app.get("/", tags=["child"])
+async def root():
+    return {"message": "Hello World"}
+
+import uvicorn
+
+uvicorn.run(app, host="0.0.0.0", port=8002, debug=False)
---
+++ Server/buildOpenApiJson.py
@@ -0,0 +1,14 @@
+import json
+
+from fastapi.openapi.utils import get_openapi
+
+from api import app
+
+with open('openapi.json', 'w') as f:
+    json.dump(get_openapi(
+        title=app.title,
+        version=app.version,
+        openapi_version=app.openapi_version,
+        description=app.description,
+        routes=app.routes,
+    ), f)
