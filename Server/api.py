696018d3a14c5137dcb38ca8e039593888bd556a
Ant-Mac <antonymitchell@hotmail.com>
2023-01-07 10:35:27 +0000

    api

--- Server/api.py
+++ Server/api.py
@@ -1,46 +1,52 @@
+from db.models import Competition, Event
 from fastapi import FastAPI
-from fastapi_quickcrud.crud_router import generic_sql_crud_router_builder
-from sqlalchemy import *
-from sqlalchemy.orm import *
+from fastapi_quickcrud.crud_router import (SqlType,
+                                           generic_sql_crud_router_builder)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
-class Parent(Base):
-    __tablename__ = 'parent_o2o'
-    id = Column(Integer, primary_key=True, comment='test-test-test')
-    name = Column(String, default='ok', unique = True)
-    children = relationship("Child", back_populates="parent")
+engine = create_engine("postgresql://pi:kayak1@192.168.0.28/aems")
+# TODO: Update to use dotenv
+session = sessionmaker(engine)
 
 
+def get_transaction_session():
+    try:
+        db = session()
+        yield db
+    finally:
+        db.close()
 
-class Child(Base):
-    __tablename__ = 'child_o2o'
-    id = Column(Integer, primary_key=True, comment='child_pk_test')
-    parent_id = Column(Integer, ForeignKey('parent_o2o.id'), info=({'description': 'child_parent_id_test'}))
-    parent = relationship("Parent", back_populates="children")
 
-
-crud_route_parent = generic_sql_crud_router_builder(
-    db_model=Parent,
-    prefix="/parent",
-    tags=["parent"],
+crud_route_competition = generic_sql_crud_router_builder(
+    db_model=Competition,
+    prefix="/competition",
+    tags=["competition"],
+    db_session=get_transaction_session,
+    sql_type=SqlType("postgresql"),
 )
 
-crud_route_child = generic_sql_crud_router_builder(
-    db_model=Child,
-    prefix="/child",
-    tags=["child"]
+crud_route_event = generic_sql_crud_router_builder(
+    db_model=Event,
+    prefix="/event",
+    tags=["event"],
+    db_session=get_transaction_session,
+    sql_type=SqlType("postgresql"),
+    foreign_include=[Competition]
 )
+
 
 app = FastAPI()
-[app.include_router(i) for i in [crud_route_parent, crud_route_child]]
+[app.include_router(i) for i in [crud_route_competition, crud_route_event]]
 
-@app.get("/", tags=["child"])
+
+@app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Go to /docs to see the swagger documentation"}
+
 
 import uvicorn
 
-# uvicorn.run(app, host="0.0.0.0", port=8002, debug=False)
 
 async def run_server():
     config = uvicorn.Config(app, host="0.0.0.0", port=8002, debug=False)
