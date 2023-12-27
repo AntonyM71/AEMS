9970cfc8fbfd5c99fee9584013773f76570aedc5
Ant-Mac <antonymitchell@hotmail.com>
2023-12-27 20:48:49 +0000

    Run prettier and make it easier to run

--- buildApi.sh
+++ buildApi.sh
@@ -4,3 +4,4 @@ python buildOpenApiJson.py
 cd ../Webapp
 npx @rtk-query/codegen-openapi src/redux/services/example_api.json
 sed -i '' 's/\| any/ /g' src/redux/services/aemsApi.ts
+npx prettier src/redux/services/aemsApi.ts -w
