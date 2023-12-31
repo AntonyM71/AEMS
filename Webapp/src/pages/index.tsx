7b9bcde0fca196a69aec0f34f2e91098c0cd4122
Ant-Mac <antonymitchell@hotmail.com>
2023-12-31 11:52:03 +0000

    Add gotchas to home page

--- Webapp/src/pages/index.tsx
+++ Webapp/src/pages/index.tsx
@@ -1,3 +1,16 @@
 export default function Score() {
-	return <div>Home</div>
+	return (
+		<div>
+			Gotchas:
+			<ul>
+				- Please don't put paddlers with different numbers of runs in
+				the same heat, it should work, but will be painful to use
+			</ul>
+			<ul>
+				- Tiebreak engine doesn't give a detailled breakdown of why yet,
+				but will present a different message if things are a complete
+				tie.
+			</ul>
+		</div>
+	)
 }
