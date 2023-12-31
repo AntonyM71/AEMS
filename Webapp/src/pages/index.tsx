66a081b2e7542e8f265d172868c4a817a236a7d1
Ant-Mac <antonymitchell@hotmail.com>
2023-12-31 12:04:47 +0000

    Update gotchas with known scoring issues

--- Webapp/src/pages/index.tsx
+++ Webapp/src/pages/index.tsx
@@ -11,6 +11,14 @@ export default function Score() {
 				but will present a different message if things are a complete
 				tie.
 			</ul>
+			<ul>
+				- If a paddler only scores one move with a single judge, they
+				will get full points for it. i.e if Judge 1 scores a spin, and
+				no other judges score any moves, they will get 10 points for the
+				spin, rather than 3.33 as you would expect if only one judge
+				scored it out of three. This situation only applies if the
+				paddler scores no moves whatsoever by one or more judges.
+			</ul>
 		</div>
 	)
 }
