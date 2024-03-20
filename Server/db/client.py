306ceb75ae615768fcb6df65eaaa5c50e42a0676
AEMS_Server <kayak.freestyle.app@gmail.com>
2024-03-20 18:29:58 +0000

    tidy up env vars

--- Server/db/client.py
+++ Server/db/client.py
@@ -7,10 +7,8 @@ from sqlalchemy.pool import SingletonThreadPool
 
 load_dotenv(".env")
 
-# database_address = "postgresql://pi:kayak1@192.168.0.28/aems"
 database_address = os.environ.get("CONNECTION_STRING")
 engine = create_engine(database_address, poolclass=SingletonThreadPool)
-# TODO: Update to use dotenv
 session = sessionmaker(engine)
 
 
