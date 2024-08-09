a89f6b06ef09407e13ce0e505b059046b6194a78
Antony <antonymitchell@hotmail.com>
2024-08-09 21:22:34 +0000

    - Improve layout of promote-phase accordian - Add explaination of how promote-phase handles ties.

--- Webapp/src/components/competition/PromotePhase.tsx
+++ Webapp/src/components/competition/PromotePhase.tsx
@@ -1,6 +1,7 @@
 import AddIcon from "@mui/icons-material/Add"
 import DeleteIcon from "@mui/icons-material/Delete"
 
+import Alert from "@mui/material/Alert"
 import Button from "@mui/material/Button"
 import Grid from "@mui/material/Grid"
 import IconButton from "@mui/material/IconButton"
@@ -82,208 +83,225 @@ export const PromotePhase = () => {
 		<Grid
 			container
 			spacing="1em"
-			width="50%"
+			width="100%"
 			alignSelf="center"
 			sx={{ padding: "1em", height: "100%" }}
 		>
-			<Grid item xs={12}>
-				<SelectorDisplay showHeat={false} />
+			<Grid item xs={10}>
+				<Alert severity="info">
+					If the cuttoff rank is tied between multiple athletes, all
+					athletes involved in the tie will progress to the next
+					round, unless their score is zero.
+				</Alert>
 			</Grid>
-			<Grid item xs={12}>
-				<TextField
-					error={!phaseName}
-					helperText={!phaseName && "Please name the Phase."}
-					label="New Phase Name"
-					variant="outlined"
-					fullWidth
-					onChange={(
-						event: React.ChangeEvent<HTMLInputElement>
-					): void => setPhaseName(event.target.value)}
-					value={phaseName}
-				/>
+			<Grid item xs={5}>
+				<SelectorDisplay showHeat={false} vertical />
 			</Grid>
-			<Grid item xs={12}>
-				<TextField
-					label="Number of Athletes to Promote"
-					variant="outlined"
-					fullWidth
-					type="number"
-					error={!numberOfAthletes}
-					helperText={
-						!numberOfAthletes &&
-						"Plese select a non-zero number of athletes."
-					}
-					onChange={(
-						event: React.ChangeEvent<HTMLInputElement>
-					): void =>
-						setNumberOfAthletes(
-							event.target.value as unknown as number
-						)
-					}
-					value={numberOfAthletes}
-				/>
-			</Grid>
-
-			<Grid item xs={12}>
-				<TextField
-					label="Number of Runs"
-					variant="outlined"
-					fullWidth
-					type="number"
-					onChange={(
-						event: React.ChangeEvent<HTMLInputElement>
-					): void =>
-						setNumberOfRuns(event.target.value as unknown as number)
-					}
-					value={numberOfRuns}
-				/>
-			</Grid>
-			<Grid item xs={12}>
-				<TextField
-					label="Number of Scoring Runs"
-					variant="outlined"
-					fullWidth
-					type="number"
-					onChange={(
-						event: React.ChangeEvent<HTMLInputElement>
-					): void =>
-						setNumberOfScoringRuns(
-							event.target.value as unknown as number
-						)
-					}
-					value={numberOfScoringRuns}
-				/>
-			</Grid>
-			<Grid item xs={12}>
-				<TextField
-					label="Number of Judges"
-					variant="outlined"
-					fullWidth
-					type="number"
-					onChange={(
-						event: React.ChangeEvent<HTMLInputElement>
-					): void =>
-						setNumberOfJudges(
-							event.target.value as unknown as number
-						)
-					}
-					value={numberOfJudges}
-				/>
-			</Grid>
-			<Grid item xs={12}>
-				<Paper sx={{ padding: "0.5em" }}>
-					<Grid container spacing={2}>
-						{newHeatNames.map((h: string, i) => (
-							<Grid item key={i} xs={12}>
-								<TextField
-									label={`New Heat: ${i + 1}`}
-									variant="outlined"
-									fullWidth
-									error={
-										!h ||
-										newHeatNames.filter((j) => j === h)
-											.length !== 1
-									}
-									helperText={
-										!h
-											? "Please give the Heat a name."
-											: newHeatNames.filter(
-													(j) => j === h
-											  ).length !== 1 &&
-											  "Multiple heats cannot have the same name."
-									}
-									onChange={(
-										event: React.ChangeEvent<HTMLInputElement>
-									): void => {
-										const updatedNewHeatNames = [
-											...newHeatNames
-										]
-										updatedNewHeatNames[i] =
-											event.target.value
-										setNewHeatNames(updatedNewHeatNames)
-									}}
-									value={h}
-									InputProps={{
-										endAdornment: (
-											<InputAdornment position="end">
-												<IconButton
-													aria-label="toggle password visibility"
-													onClick={() => {
-														const updatedNewHeatNames =
-															newHeatNames.filter(
-																(item) =>
-																	item !== h
-															)
-														setNewHeatNames(
-															updatedNewHeatNames
-														)
-													}}
-													edge="end"
-												>
-													<DeleteIcon />
-												</IconButton>
-											</InputAdornment>
-										)
-									}}
-								/>
-							</Grid>
-						))}
-						<Grid item xs={12}>
-							{" "}
-							<TextField
-								label="New Heat Name"
-								variant="outlined"
-								fullWidth
-								value={newHeatName}
-								helperText={
-									newHeatNames.length === 0 &&
-									"Please add at least one heat"
-								}
-								onChange={(
-									event: React.ChangeEvent<HTMLInputElement>
-								): void => {
-									setNewHeatName(event.target.value)
-								}}
-								onKeyUp={(event) => {
-									if (event.key === "Enter") {
-										handleAddNewHeat()
-									}
-								}}
-								InputProps={{
-									endAdornment: (
-										<InputAdornment position="end">
-											<IconButton
-												aria-label="toggle password visibility"
-												onClick={() => {
-													handleAddNewHeat()
-												}}
-												edge="end"
-											>
-												<AddIcon />
-											</IconButton>
-										</InputAdornment>
-									)
-								}}
-							/>
-						</Grid>
+			<Grid item xs={5}>
+				<Grid container spacing="1em">
+					<Grid item xs={12}>
+						<TextField
+							error={!phaseName}
+							helperText={!phaseName && "Please name the Phase."}
+							label="New Phase Name"
+							variant="outlined"
+							fullWidth
+							onChange={(
+								event: React.ChangeEvent<HTMLInputElement>
+							): void => setPhaseName(event.target.value)}
+							value={phaseName}
+						/>
 					</Grid>
-				</Paper>
-			</Grid>
-
-			<Grid item xs={12}>
-				<Button
-					variant="contained"
-					fullWidth
-					disabled={
-						!phaseName ||
-						!numberOfAthletes ||
-						newHeatNames.length === 0 ||
-						!phaseId
-					}
-					onClick={() => void submitForm()}
-				>
-					Create Phase
-				</Button>
+					<Grid item xs={12}>
+						<TextField
+							label="Number of Athletes to Promote"
+							variant="outlined"
+							fullWidth
+							type="number"
+							error={!numberOfAthletes}
+							helperText={
+								!numberOfAthletes &&
+								"Plese select a non-zero number of athletes."
+							}
+							onChange={(
+								event: React.ChangeEvent<HTMLInputElement>
+							): void =>
+								setNumberOfAthletes(
+									event.target.value as unknown as number
+								)
+							}
+							value={numberOfAthletes}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							label="Number of Runs"
+							variant="outlined"
+							fullWidth
+							type="number"
+							onChange={(
+								event: React.ChangeEvent<HTMLInputElement>
+							): void =>
+								setNumberOfRuns(
+									event.target.value as unknown as number
+								)
+							}
+							value={numberOfRuns}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							label="Number of Scoring Runs"
+							variant="outlined"
+							fullWidth
+							type="number"
+							onChange={(
+								event: React.ChangeEvent<HTMLInputElement>
+							): void =>
+								setNumberOfScoringRuns(
+									event.target.value as unknown as number
+								)
+							}
+							value={numberOfScoringRuns}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							label="Number of Judges"
+							variant="outlined"
+							fullWidth
+							type="number"
+							onChange={(
+								event: React.ChangeEvent<HTMLInputElement>
+							): void =>
+								setNumberOfJudges(
+									event.target.value as unknown as number
+								)
+							}
+							value={numberOfJudges}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<Paper sx={{ padding: "0.5em" }}>
+							<Grid container spacing={2}>
+								{newHeatNames.map((h: string, i) => (
+									<Grid item key={i} xs={12}>
+										<TextField
+											label={`New Heat: ${i + 1}`}
+											variant="outlined"
+											fullWidth
+											error={
+												!h ||
+												newHeatNames.filter(
+													(j) => j === h
+												).length !== 1
+											}
+											helperText={
+												!h
+													? "Please give the Heat a name."
+													: newHeatNames.filter(
+															(j) => j === h
+													  ).length !== 1 &&
+													  "Multiple heats cannot have the same name."
+											}
+											onChange={(
+												event: React.ChangeEvent<HTMLInputElement>
+											): void => {
+												const updatedNewHeatNames = [
+													...newHeatNames
+												]
+												updatedNewHeatNames[i] =
+													event.target.value
+												setNewHeatNames(
+													updatedNewHeatNames
+												)
+											}}
+											value={h}
+											InputProps={{
+												endAdornment: (
+													<InputAdornment position="end">
+														<IconButton
+															aria-label="toggle password visibility"
+															onClick={() => {
+																const updatedNewHeatNames =
+																	newHeatNames.filter(
+																		(
+																			item
+																		) =>
+																			item !==
+																			h
+																	)
+																setNewHeatNames(
+																	updatedNewHeatNames
+																)
+															}}
+															edge="end"
+														>
+															<DeleteIcon />
+														</IconButton>
+													</InputAdornment>
+												)
+											}}
+										/>
+									</Grid>
+								))}
+								<Grid item xs={12}>
+									{" "}
+									<TextField
+										label="New Heat Name"
+										variant="outlined"
+										fullWidth
+										value={newHeatName}
+										helperText={
+											newHeatNames.length === 0 &&
+											"Please add at least one heat"
+										}
+										onChange={(
+											event: React.ChangeEvent<HTMLInputElement>
+										): void => {
+											setNewHeatName(event.target.value)
+										}}
+										onKeyUp={(event) => {
+											if (event.key === "Enter") {
+												handleAddNewHeat()
+											}
+										}}
+										InputProps={{
+											endAdornment: (
+												<InputAdornment position="end">
+													<IconButton
+														aria-label="toggle password visibility"
+														onClick={() => {
+															handleAddNewHeat()
+														}}
+														edge="end"
+													>
+														<AddIcon />
+													</IconButton>
+												</InputAdornment>
+											)
+										}}
+									/>
+								</Grid>
+							</Grid>
+						</Paper>
+					</Grid>
+					<Grid item xs={12}>
+						<Button
+							variant="contained"
+							fullWidth
+							disabled={
+								!phaseName ||
+								!numberOfAthletes ||
+								newHeatNames.length === 0 ||
+								!phaseId
+							}
+							onClick={() => void submitForm()}
+						>
+							Create Phase
+						</Button>
+					</Grid>{" "}
+				</Grid>
 			</Grid>
 		</Grid>
 	)
