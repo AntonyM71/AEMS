71370f3dc782c7bb70af59accb6aa2dd7f9960dd
Antony <antonymitchell@hotmail.com>
2024-08-14 22:16:09 +0000

    Improve exceptions to use structlog and rich

--- Server/main.py
+++ Server/main.py
@@ -1,6 +1,13 @@
+import os
+import time
+
+import structlog
 import uvicorn
-from fastapi import FastAPI
+from asgi_correlation_id import CorrelationIdMiddleware
+from asgi_correlation_id.context import correlation_id
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import parse_obj_as
 
 from app.autogenEndpoints import (
     crud_route_athlete,
@@ -21,9 +28,18 @@ from app.competition_management.competition_management import (
 from app.competition_management.pdfEndpoints import pdf_router
 from app.scoresheetEndpoints import scoresheet_router
 from app.scoring.customScoringEndpoints import scoring_router
+from custom_logging import setup_logging
 
 frontend_url = "http://localhost:3000"
 request_origins = [frontend_url]
+
+
+LOG_JSON_FORMAT = parse_obj_as(bool, os.getenv("LOG_JSON_FORMAT", False))
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+setup_logging(json_logs=LOG_JSON_FORMAT, log_level=LOG_LEVEL)
+
+access_logger = structlog.stdlib.get_logger("api.access")
+
 
 app = FastAPI()
 [
@@ -48,6 +64,55 @@ app = FastAPI()
 ]
 
 
+@app.middleware("http")
+async def logging_middleware(request: Request, call_next) -> Response:
+    structlog.contextvars.clear_contextvars()
+    # These context vars will be added to all log entries emitted during the request
+    request_id = correlation_id.get()
+    structlog.contextvars.bind_contextvars(request_id=request_id)
+
+    start_time = time.perf_counter_ns()
+    # If the call_next raises an error, we still want to return our own 500 response,
+    # so we can add headers to it (process time, request ID...)
+    response = Response(status_code=500)
+    try:
+        response = await call_next(request)
+    except Exception:
+        # TODO: Validate that we don't swallow exceptions (unit test?)
+        structlog.stdlib.get_logger("api.error").exception("Uncaught exception")
+        raise
+    finally:
+        process_time = time.perf_counter_ns() - start_time
+        status_code = response.status_code
+        url = request.base_url
+        client_host = request.client.host
+        client_port = request.client.port
+        http_method = request.method
+        http_version = request.scope["http_version"]
+        # Recreate the Uvicorn access log format, but add all parameters as structured information
+        access_logger.info(
+            f"""{client_host}:{client_port} - "{http_method} {url} HTTP/{http_version}" {status_code}""",
+            http={
+                "url": str(request.url),
+                "status_code": status_code,
+                "method": http_method,
+                "request_id": request_id,
+                "version": http_version,
+            },
+            network={"client": {"ip": client_host, "port": client_port}},
+            duration=process_time,
+        )
+        response.headers["X-Process-Time"] = str(process_time / 10**9)
+        return response
+
+
+# This middleware must be placed after the logging, to populate the context with the request ID
+# NOTE: Why last??
+# Answer: middlewares are applied in the reverse order of when they are added (you can verify this
+# by debugging `app.middleware_stack` and recursively drilling down the `app` property).
+app.add_middleware(CorrelationIdMiddleware)
+
+
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Go to /docs to see the swagger documentation"}
@@ -63,6 +128,6 @@ app.add_middleware(
 
 
 async def run_server() -> None:
-    config = uvicorn.Config(app, host="0.0.0.0", port=8000)
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000, log_config=None)
     server = uvicorn.Server(config)
     await server.serve()
