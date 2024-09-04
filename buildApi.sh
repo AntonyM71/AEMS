6b29041a0ed52192e5559803b9f224bfa06b6376
AEMS <kayak.freestyle.app@gmail.com>
2024-09-04 10:41:51 +0000

    tweak command

--- buildApi.sh
+++ buildApi.sh
@@ -7,8 +7,8 @@ npx @rtk-query/codegen-openapi src/redux/services/example_api.json
 if sed --version 2>/dev/null | grep -q GNU; then
     SED_CMD="sed -i"
 else
-    SED_CMD = "sed -i ''"
+    SED_CMD="sed -i ''"
 fi
-$SED_CMD 's/| any//g' src/redux/services/aemsApi.ts
+$SED_CMD 's/| any/ /g' src/redux/services/aemsApi.ts
 
 npx prettier src/redux/services/aemsApi.ts -w
