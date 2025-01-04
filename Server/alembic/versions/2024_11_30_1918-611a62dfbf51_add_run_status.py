317dabf8adc25ad130318173dec501f7ee95a9a7
Ant <kayak.freestyle.app@gmail.com>
2025-01-04 14:54:36 +0000

    Ruff check fix

--- Server/alembic/versions/2024_11_30_1918-611a62dfbf51_add_run_status.py
+++ Server/alembic/versions/2024_11_30_1918-611a62dfbf51_add_run_status.py
@@ -6,9 +6,10 @@ Create Date: 2024-11-30 19:18:29.441936
 
 """
 
-from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
+
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "611a62dfbf51"
