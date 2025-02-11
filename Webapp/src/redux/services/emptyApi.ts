ca87e5e19f8295593792ae5804e934747d31a475
Ant <kayak.freestyle.app@gmail.com>
2025-02-11 17:44:58 +0000

    Fix fundemental type issues

--- Webapp/src/redux/services/emptyApi.ts
+++ Webapp/src/redux/services/emptyApi.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react"
 
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
+	reducerPath: "aemsApi",
 	baseQuery: retry(
 		fetchBaseQuery({
 			baseUrl: process.env.NEXT_PUBLIC_API_URL_DEV ?? "/api/"
