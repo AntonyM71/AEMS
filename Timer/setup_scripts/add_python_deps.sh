b29c8edcb92bf787ac94c10aa7cb1b229b847b41
Antony Mitchell <kayakfreestyleapp@gmail.com>
2025-03-30 13:00:09 +0100

    Add python deps

--- Timer/setup_scripts/add_python_deps.sh
+++ Timer/setup_scripts/add_python_deps.sh
@@ -3,4 +3,5 @@ sudo apt-get install python3-pip
 sudo apt-get install python3-pil
 sudo apt-get install python3-numpy
 sudo apt-get install python3-spidev
+sudo apt-get install python3-websockets
 sudo apt install python3-gpiozero
